36d2fef1fc34fce781e2eacaf1e48912b926c3b1
Camilo López <lopezquekk@gmail.com>
2025-12-29 22:36:40 -0500

    init

---
+++ Wims/WimsTests/WimsTests.swift
@@ -0,0 +1,17 @@
+//
+//  WimsTests.swift
+//  WimsTests
+//
+//  Created by Camilo Lopez on 12/29/25.
+//
+
+import Testing
+@testable import Wims
+
+struct WimsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
